ed0f5103b3ee6d575bc8e5e4ab97477fa5105a0c
silk <silk@khel.dar>
2017-03-19 20:48:11 -0700

    Added a Shader class. Removed and replaced old vertex and fragment shaders

---
+++ Buriza/src/Shader.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    ~Shader() = default;
+    void Use();
+
+private:
+    GLuint m_program;
+};
+
--- Buriza/src/main.cpp
+++ Buriza/src/main.cpp
@@ -4,17 +4,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-const char* vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 position;\n"
-                                 "void main() {\n"
-                                 "    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-                                 "}";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 color;\n"
-                                   "void main() {\n"
-                                   "    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                   "}";
+#include "Shader.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
@@ -87,43 +77,13 @@ int main()
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
 
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cerr << "Vertex shader compilation failed!" << std::endl << infoLog << std::endl;
-    }
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cerr << "Fragment shader compilation failed!" << std::endl << infoLog << std::endl;
-    }
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cerr << "Shader program linking failed!" << std::endl << infoLog << std::endl;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
+
+    Shader ourShader("src/shaders/shader.vs", "src/shaders/shader.fs");
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -131,7 +91,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        ourShader.Use();
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
         glBindVertexArray(0);
