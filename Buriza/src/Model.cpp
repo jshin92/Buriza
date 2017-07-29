30e1910b5cbde6c7343228306ad8a760dca54bb5
silk <silk@khel.dar>
2017-07-28 17:47:59 -0700

    fleshed out more of TextureFromFile, need to substitute in stb_image soon...

--- Buriza/src/Model.cpp
+++ Buriza/src/Model.cpp
@@ -6,10 +6,7 @@
 
 namespace
 {
-    GLuint TextureFromFile(const char* path, const std::string& directory, bool gamma=false)
-    {
-        return 0;
-    }
+    GLuint TextureFromFile(const char* path, const std::string& directory, bool gamma=false);
 }
 
 Model::Model(const char* path)
@@ -124,4 +121,43 @@ std::vector<Texture> Model::LoadMaterialTextures(aiMaterial* mat, aiTextureType 
         textures.emplace_back(texture);
     }
     return textures;
+}
+
+namespace
+{
+    GLuint TextureFromFile(const char* path, const std::string& directory, bool gamma)
+    {
+        std::string filename = std::string(path);
+        filename = directory + '/' + filename;
+
+        GLuint textureID;
+        glGenTextures(1, &textureID);
+
+        GLint width, height, numComponents;
+        //GLubyte* data = stbi_load(filename.c_str(), &width, &height, &numComponents, 0);
+        GLubyte* data = nullptr;
+        if (data)
+        {
+            GLenum format;
+            if (numComponents == 1) format = GL_RED;
+            else if (numComponents == 3) format = GL_RGB;
+            else if (numComponents == 4) format = GL_RGBA;
+            else std::cerr << "Unmatched numComponents: " << numComponents << std::endl;
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        }
+        else
+        {
+            std::cerr << "Texture failed to load at path: " << path << std::endl;
+        }
+        //stbi_image_free(data);
+        return textureID;
+    }
 }
