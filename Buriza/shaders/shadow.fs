137d50db0177d3d9a221e5c23597b2ad0e308cc4
silk <silk@khel.dar>
2018-05-26 21:35:37 -0700

    implement rudimentary pcf to get softer shadows

--- Buriza/shaders/shadow.fs
+++ Buriza/shaders/shadow.fs
@@ -26,9 +26,17 @@ float shadowCalculation(vec4 fragPosLightSpace) {
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float bias = max(0.05 * (1.0 - dot(fs_in.Normal, lightDir)), 0.005);
-    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for (int x = -1; x <=1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
+        }
+    }
+
     if (projCoords.z > 1.0) shadow = 0.0;
-    return shadow;
+    return shadow / 9.0;
 }
 
 void main() {
