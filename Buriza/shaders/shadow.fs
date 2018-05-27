2066d576e0ec52f74cb64df11c7e7f4c0f416d46
silk <silk@khel.dar>
2018-05-26 23:38:55 -0700

    reduce peter panning

--- Buriza/shaders/shadow.fs
+++ Buriza/shaders/shadow.fs
@@ -25,7 +25,7 @@ float shadowCalculation(vec4 fragPosLightSpace) {
     float currentDepth = projCoords.z;
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
-    float bias = max(0.05 * (1.0 - dot(fs_in.Normal, lightDir)), 0.005);
+    float bias = max(0.02 * (1.0 - dot(fs_in.Normal, lightDir)), 0.005);
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
     for (int x = -1; x <=1; ++x) {
--- Buriza/src/main.cpp
+++ Buriza/src/main.cpp
@@ -53,6 +53,7 @@ int main()
         return -1;
     }
 
+    glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -114,12 +115,13 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         /* first render to depth map */
+        glCullFace(GL_FRONT);
         glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
         glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
         glClear(GL_DEPTH_BUFFER_BIT);
         // configure shader and matrices
         GLfloat near_plane = 1.0f, far_plane = 7.5f;
-        glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+        glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, 10.0f, -10.0f, near_plane, far_plane);
         glm::mat4 lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f),
                                           glm::vec3( 0.0f, 0.0f, 0.0f),
                                           glm::vec3( 0.0f, 1.0f, 0.0f));
@@ -132,6 +134,7 @@ int main()
         hero.Draw(simpleDepthShader);
 
         // then render the scene 
+        glCullFace(GL_BACK);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
