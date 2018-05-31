4b0a7700b906f399db750b11b4f953f3b6760455
silk <silk@khel.dar>
2018-05-30 18:47:21 -0700

    no need to use std::get inside main to fetch shadowPassOutput

--- Buriza/src/main.cpp
+++ Buriza/src/main.cpp
@@ -106,7 +106,7 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        auto shadowOutput = std::get<ShadowPassOutput>(shadowPass.Run({}));
+        auto shadowOutput = shadowPass.Run({});
         plane.Draw(simpleDepthShader);
         cube.Draw(simpleDepthShader);
         hero.Draw(simpleDepthShader);
